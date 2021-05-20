bc29f1bb269fee83ba4f2efbf5b96bbcc0e9c049
industrious-agent-153d <klem.noah@gmail.com>
2021-05-20 15:20:38 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if (board[index] == " X " || board[index] == "X")
+    TRUE
+    
+    elsif (board[index] == " O " || board[index] == "O")
+    TRUE
+    
+  else (board[index] == " " || board[index] == "" || board[index] == nil)
+    FALSE
+  end
+end
